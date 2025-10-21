6adf7b975e5b600459076f64f2ab098582645818
jofisena <anajosefinamigotti@gmail.com>
2025-10-21 16:21:16 -0300

    Correción - comentarios

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // 1. Estructura de datos (Array) para páginas
-    // (Usamos rutas absolutas desde la raíz de tu sitio)
+    // datos pag - navbar */
     const paginasDelSitio = [
       { titulo: 'Inicio', url: '/index.html' },
       { titulo: 'Tienda', url: '/pages/tienda.html' },
@@ -9,51 +8,37 @@ document.addEventListener('DOMContentLoaded', () => {
       { titulo: 'Login', url: '/pages/login.html' },
       { titulo: 'Registro', url: '/pages/registro.html' }
     ];
-
-    // 2. Busca el contenedor del navbar en el HTML
     const navButtonsContainer = document.querySelector('.nav-buttons-group');
 
-    // 3. Verifica si el contenedor existe en esta página
     if (navButtonsContainer) {
-        
-        // Dibuja los enlaces del array
         paginasDelSitio.forEach(pagina => {
             const link = document.createElement('a');
             link.href = pagina.url;
             link.textContent = pagina.titulo;
-            
-            // Asigna las clases CSS que ya tenías
+            // estilo btn
             link.classList.add('nav-button');
-            
-            // Estilo especial para el botón de Inicio
             if (pagina.titulo === 'Inicio') {
                 link.classList.add('dark-button', 'home-button');
             }
-            
             navButtonsContainer.appendChild(link);
         });
 
-        // 4. Crea y añade dinámicamente el botón de Logout
+        // dinámica - logout 
         const logoutButton = document.createElement('a');
         logoutButton.href = '#';
         logoutButton.id = 'btn-logout';
         logoutButton.textContent = 'Cerrar Sesión';
-        logoutButton.classList.add('nav-button'); // Dale el mismo estilo
+        // estilo btn
+        logoutButton.classList.add('nav-button'); 
         navButtonsContainer.appendChild(logoutButton);
 
-        // 5. Asigna la lógica de Logout (Consigna 2)
+        // lógica btn
         logoutButton.addEventListener('click', (event) => {
-            // Previene que el enlace '#' recargue la página
             event.preventDefault(); 
-            
-            // ¡IMPORTANTE! Usa la "llave" real que guardaste en el login
             localStorage.removeItem('usuarioLogueado'); 
-            
             alert("Has cerrado la sesión.");
-            // Redirige al login
             window.location.href = '/pages/login.html';
         });
-        
-    } // Fin del if (navButtonsContainer)
+    } 
 
 });
